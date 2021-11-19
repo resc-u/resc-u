deac6369f717902b3ea69a8b228d9eff3eddac32
nicomellon <nmellon94@gmail.com>
2021-11-19 18:16:21 +0100

    animal and adopter models

---
+++ models/Animal.model.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const animalSchema = new Schema({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  sex: {
+    type: String,
+    required: true,
+    enum: ["male", "female"],
+  },
+  size: {
+    type: String,
+    enum: ["small", "medium", "large"],
+  },
+  age: { type: Number },
+  status: {
+    type: String,
+    enum: ["awaiting adoption", "adopted"],
+  },
+  color: { type: String },
+  breed: { type: String },
+  dateofentry: { type: Date, default: Date.now },
+  images: { type: [String] },
+  kidfriendly: { type: Boolean },
+  shelter: { type: Schema.Types.ObjectId, ref: "Shelter" },
+});
+
+const Animal = model("Animal", animalSchema);
+module.exports = Animal;
---
+++ models/User.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const userOptions = {
+  discriminatorKey: "usertype", // our discriminator key, could be anything
+  collection: "users", // the name of our collection
+};
+
+// Our Base schema: these properties will be shared with our "real" schemas
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema(
+    {
+      name: { type: String, required: true },
+      password: { type: String, required: true },
+      email: { type: String, required: true, unique: true },
+    },
+    userOptions
+  )
+);
+
+module.exports = mongoose.model("User");
