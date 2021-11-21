31b615d2f2162b80c23143af0efeef5df6126605
Monica Saiz <moni.sm@gmail.com>
2021-11-21 22:59:18 +0100

    Showing profile depending on user Role, and creating User after signup using discriminator

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt")
 const User = require("../models/User.model")
+const Adopter = require("../models/Adopter.model")
+const Shelter = require("../models/Shelter.model")
 
 
 /* /signup sister routes */
@@ -10,6 +12,8 @@ router
     .post( async (req, res) => {
         
         let error = null
+        let isAdopter = false
+        let isShelter = false
         let newUser = null
         let { username, email, password, role } = req.body
         
@@ -25,17 +29,21 @@ router
           const salt = bcrypt.genSaltSync(4)
           const hashedPwd = bcrypt.hashSync(password, salt)
 
-          newUser = await User.create({ username, email, role, password: hashedPwd })
+          if (role === "adopter") {
+            newUser = await Adopter.create({ username, email, role, password: hashedPwd })
+            isAdopter = true
+          } else if (role === "shelter") {
+            newUser = await Shelter.create({ username, email, role, password: hashedPwd })
+            isShelter = true
+          }
 
         } catch (e) {
-
           error = { errType: "DB_ERR", message: e }
-          console.log("ERROR:", e)
           
         } finally {
-
-          if (newUser) res.render("index", { loggedInUser: newUser, username, email, role, message: "User created successfully!!" })
-          else res.render("signup", { username, email, role, error: { type: "USER_ERROR", message: "This user already exists!" }})
+          
+          if (newUser) res.render("users/profile", { loggedInUser: newUser, username, email, role, isAdopter, isShelter, message: "User created successfully!!" })
+          else res.render("signup", { username, email, role, error: { type: "DB_ERROR", message: "Error in the DB" }})
           
         }
   })
@@ -47,7 +55,6 @@ router
     .get((req, res ) => { res.render('index') })
     .post( async (req, res ) => {
       
-      let isPwdCorrect = false
       let loggedInUser = null
       let error = null
       let message = ""
@@ -59,17 +66,17 @@ router
 
         const {email, password} = req.body 
         if (!email || !password) res.render('index', { error: {type: "CREDENTIALS_ERROR", message: "Wrong credentials!" }})
-    
+            
         loggedInUser = await User.findOne( { email } )
         if (!loggedInUser) res.render('index', { error: {type: "USER_ERROR", message: "User doesn't exists!" }})
     
-        isPwdCorrect = bcrypt.compareSync(password, loggedInUser.password)
+        let isPwdCorrect = bcrypt.compareSync(password, loggedInUser.password)
         
         if (isPwdCorrect) {
           //req.session.loggedInUser = loggedInUser
           message = "You are logged in!"
 
-          switch(loggedInUser.role) {
+          switch(loggedInUser.role) { 
             case "adopter": isAdopter = true
               break;
             case "shelter": isShelter = true
@@ -86,15 +93,15 @@ router
           error = { errType: "DB_ERR", message: e }
 
       } finally {
-          res.render("users/profile", { loggedInUser, message, error, isAdopter, isAdmin, isShelter })
+          res.render("users/profile", { loggedInUser, user: loggedInUser, message, error, isAdopter, isAdmin, isShelter })
       }
     })
 
 
 router.get('/logout', (req, res) => {
   req.session.destroy((err) => {
-      if (err) res.redirect('/');
-      else res.render('index', { message: "You are logged out!" });
+      if (err) res.redirect('/')
+      else res.render('index', { message: "You are logged out!" })
   });
 });
 
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -19,36 +19,98 @@ router
     })
 
 // GET and POST /users/profile/edit/:id (sister routes)     
-router
-    .route('/profile/edit/:id')
-    .get( async (req, res) => { 
+router.get('/profile/edit/:role/:id', async (req, res) => { 
         
-        let user = null
+    let user = null
+    let { id, role } = req.params
 
-        try {
+    try {
 
-            user = await User.findById(req.params.id)
-            if (!user) res.render('users/profile', { error: {type: "USER_ERROR", message: "User not found!" }})
-            
-
-        } catch (e) {
-            error = { errType: "DB_ERR", message: e }
-
-        } finally {
-
-            switch(user.role) {
-                case "adopter": res.render('adopters/edit-profile', { user })
-                    break;
-                case "shelter": res.render('shelters/edit-profile', { user })
-                    break;
-                default:        res.render('admin/control-panel', { user })
-            }
-        } 
-    })
-    .get( async (req, res) => { 
+        user = await User.findById(id)
+        if (!user) res.render('users/profile', { error: {type: "USER_ERROR", message: "User not found!" }})
         
-        // TO DO
-    })
+        // Logged user can only edit their own profile (unless they are admin!)
+        //if (loggedInUser.role === 'admin' || loggedInUser.id === req.params.id) {
+            /// TO DO WHEN WE ARE HANDLING THE SESSIONS
+        //}
+
+    } catch (e) {
+        error = { errType: "DB_ERR", message: e }
+
+    } finally {
+
+        switch(role) {
+            case "adopter": res.render('adopters/edit-profile', { user })
+                break;
+            case "shelter": res.render('shelters/edit-profile', { user })
+                break;
+            default:        res.render('admin/control-panel', { user })
+        }
+    } 
+})
+
+// Different routes for each role for "POST Edit Profile" (because the response is very different)
+
+// POST /profile/edit/adopter/:id
+router.post('/profile/edit/adopter/:id', async (req, res) => { 
+        
+    let { fullname, children, animalPreference, housingSize } = req.body
+    let userToUpdate = null
+    let updatedUser = null
+
+    try {
+
+        console.log(req.params.id)
+        console.log("req.body",  req.body)
+
+        if (!fullname) res.render('profile/edit/:id', { error: {type: "FORM_ERROR", message: "Fullname is required." }})
+        
+        userToUpdate = await User.findById(req.params.id)
+        console.log("user to update", userToUpdate)
+        if (!userToUpdate) res.render(`profile/edit/adopter/${req.params.id}`, { error: {type: "DB_ERROR", message: "Error in the DB" }})
+       
+        updatedUser = await User.findByIdAndUpdate(req.params.id, 
+                                                    { fullname: "Monica Saiz Martinez", 
+                                                    children: 0, 
+                                                    animalPreference: "dog", 
+                                                    housingSize: "small appartment", 
+                                                    new: true })
+
+        console.log("updatedUser", updatedUser)
+
+    } catch (e) {
+        error = { errType: "DB_ERR", message: e }
+    } finally {
+
+        res.render('users/profile', { user: updatedUser, isAdopter: true, loggedInUser: updatedUser })
+    
+    }
+})  
+
+// GET /profile/:id
+router.get('/profile/:id', async (req, res) => {
+    let user = null
+    let isAdopter = false
+    let isShelter = false
+    let isAdmin = false
+
+    try {
+        user = await User.findById(req.params.id)
+        switch(loggedInUser.role) {
+            case "adopter": isAdopter = true
+              break;
+            case "shelter": isShelter = true
+              break;
+            default: isAdmin = true
+          }
+
+    } catch (e) {
+        error = { errType: "DB_ERR", message: e }
+    } finally {
+        res.render('/profile', {user, loggedInUser: user, isAdopter, isShelter, isAdmin})
+    }
+
+ })  
 
 
 module.exports = router;
