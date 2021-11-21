e3f54232ce4b4c65f28234f3831b38ea4d7974a5
nicomellon <nmellon94@gmail.com>
2021-11-21 12:09:53 +0100

    basic DB setup

--- db/index.js
+++ db/index.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/project-resc-u-test";
+require("dotenv").config();
+
+const MONGO_URI = process.env.MONGODB_URI;
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
