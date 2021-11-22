170d15145f7e0a24cacb80ade014578b5515c9fa
nicomellon <nmellon94@gmail.com>
2021-11-22 10:11:54 +0100

    database setup, started session setup

--- db/index.js
+++ db/index.js
@@ -1,18 +1,29 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const MONGO_URI = process.env.MONGODB_URI;
+const uri = "mongodb+srv://user:pass@resc-u.okafj.mongodb.net/resc-u";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: "admin",
+  pass: "1234",
+};
 
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
+mongoose.connect(uri, options);
+const db = mongoose.connection;
+
+// When successfully connected
+db.once("connected", () => console.log("Database connection open"));
+
+// When the connection is disconnected
+db.on("disconnected", () => console.log("Database connection disconnected"));
+
+// If the connection throws an error
+db.on("error", (err) => console.error(`Database connection error: ${err}`));
+
+// If the Node process ends, close the Mongoose connection
+process.on("SIGINT", () => {
+  db.close(() => {
+    console.log("Database connection disconnected through app termination");
+    process.exit(0);
   });
+});
--- db/seeds-test-users.js
+++ db/seeds-test-users.js
@@ -1,42 +1,69 @@
-
 const mongoose = require("mongoose");
 const User = require("../models/User.model");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+
+const uri = "mongodb+srv://user:pass@resc-u.okafj.mongodb.net/resc-u";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: "admin",
+  pass: "1234",
+};
+
+// When successfully connected
+mongoose.connection.on("connected", () =>
+  console.log("Mongoose default connection open")
+);
+
+// When the connection is disconnected
+mongoose.connection.on("disconnected", () =>
+  console.log("Mongoose default connection disconnected")
+);
+
+// If the connection throws an error
+mongoose.connection.on("error", (err) =>
+  console.log(`Mongoose default connection error: ${err}`)
+);
 
 const users = [
-    {
-        username: "test-adopter",
-        password: bcrypt.hashSync("1234", bcrypt.genSaltSync(4)),
-        email: "adopter@test.com",
-        role: "adopter"
-    },
-    {
-        username: "test-shelter",
-        password: bcrypt.hashSync("1234", bcrypt.genSaltSync(4)),
-        email: "shelter@test.com",
-        role: "shelter"
-    },
-    {
-        username: "test-admin",
-        password: bcrypt.hashSync("1234", bcrypt.genSaltSync(4)),
-        email: "admin@test.com",
-        role: "admin"
-    }
+  {
+    username: "test-adopter",
+    password: bcrypt.hashSync("1234", bcrypt.genSaltSync(4)),
+    email: "adopter@test.com",
+    role: "adopter",
+  },
+  {
+    username: "test-shelter",
+    password: bcrypt.hashSync("1234", bcrypt.genSaltSync(4)),
+    email: "shelter@test.com",
+    role: "shelter",
+  },
+  {
+    username: "test-admin",
+    password: bcrypt.hashSync("1234", bcrypt.genSaltSync(4)),
+    email: "admin@test.com",
+    role: "admin",
+  },
 ];
 
-// connects to DB
-require('./index');
+const seedDB = async () => {
+  try {
+    // connect to DB
+    mongoose.connect(uri, options);
 
-User.deleteMany()
-.then(deletedUsers =>
-  console.log(`Deleted ${deletedUsers.deletedCount} testing users`)
-)
-.then(
-  User.insertMany(users)
-  .then(insertedUsers => {
-    console.log(`Created ${insertedUsers.length} testing users`)
-    mongoose.connection.close()}
-  ))
-.catch(err =>
-   console.log(`An error occurred seeding testing users to the DB: ${err}`)
-)
+    // delete all users in DB
+    const usersInDb = await User.find();
+    const deletedUsers = await User.deleteMany({ usersInDb });
+    console.log(`Deleted ${deletedUsers.deletedCount} testing users`);
+
+    // insert test users in DB
+    const insertedUsers = await User.insertMany(users);
+    console.log(`Created ${insertedUsers.length} testing users`);
+  } catch (err) {
+    console.log(`An error occurred seeding testing users to the DB: ${err}`);
+  } finally {
+    mongoose.disconnect();
+  }
+};
+
+seedDB();
