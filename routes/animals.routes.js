e74e3e2dc2f6fe51d6b69e9f9148ae60aa6d95c9
Carlos Fung <carlos.fung@me.com>
2021-11-23 17:01:23 +0100

    last update

--- routes/animals.routes.js
+++ routes/animals.routes.js
@@ -6,6 +6,8 @@ const Animal = require("../models/Animal.model.js");
 // ********* require fileUploader in order to use it *********
 const fileUploader = require("../config/cloudinary.config");
 
+
+
 router.get("/", (req, res) => {
   Animal.find()
     .then((animalsFromDB) => {
@@ -17,48 +19,123 @@ router.get("/", (req, res) => {
 });
 
 router.route("/new").get((req, res) => {
-  res.render("animals/new-animal.hbs");
-});
-// POST route for saving a new animal in the database
-// This route has the image upload example
-router.post("/new", fileUploader.array("animal-image[]", 3), (req, res) => {
-  const {
-    name,
-    description,
-    type,
-    sex,
-    size,
-    age,
-    status,
-    color,
-    breed,
-    dateofentry,
-    kidfriendly,
-  } = req.body;
-
-  Animal.create({
-    name,
-    description,
-    type,
-    sex,
-    size,
-    age,
-    status,
-    color,
-    breed,
-    dateofentry,
-    kidfriendly,
-    imageUrl: req.files,
-    shelter: req.session.loggedInUser._id,
-  })
-    .then((newlyCreatedAnimalFromDB) => {
-      console.log(req.files);
-      console.log(newlyCreatedAnimalFromDB);
-      res.render("animals/new-animal.hbs");
+    res.render("animals/new-animal.hbs");
+})
+.post( fileUploader.array("animal-image[]", 3), (req, res) => {
+    const {
+      name,
+      description,
+      type,
+      sex,
+      size,
+      age,
+      status,
+      color,
+      breed,
+      dateofentry,
+      kidfriendly,
+    } = req.body;
+  
+    Animal.create({
+      name,
+      description,
+      type,
+      sex,
+      size,
+      age,
+      status,
+      color,
+      breed,
+      dateofentry,
+      kidfriendly,
+      imageUrl: req.files,
+      shelter: req.session.loggedInUser._id,
     })
-    .catch((error) =>
-      console.log(`Error while creating a new animal: ${error}`)
-    );
+      .then((newlyCreatedAnimalFromDB) => {
+        console.log(req.files);
+        console.log(newlyCreatedAnimalFromDB);
+        res.redirect("/shelters/animals");
+      })
+      .catch((error) =>
+        console.log(`Error while creating a new animal: ${error}`)
+      );
+  });
+
+
+router.get("/:id", (req, res) => {
+    Animal.findById(req.params.id, function (err, foundbyid){
+        if (err) { return console.log(err)}
+        res.render("animals/animal-page.hbs", foundbyid)
+    })
+
+})
+
+router.get("/edit/:id", (req, res) => {
+    let id = req.params.id
+    Animal.findById(id, function (err, foundbyid){
+        if (err) { return console.log(err)}
+        res.render("animals/animal-edit.hbs", foundbyid)
+    })
 });
+router.post("/edit/:id", fileUploader.array("animal-image[]", 3), (req, res) => {
+      
+    const {
+      name,
+      description,
+      type,
+      sex,
+      size,
+      age,
+      status,
+      color,
+      breed,
+      dateofentry,
+      kidfriendly,
+    } = req.body;
+
+ let id = req.params.id
+
+    Animal.findOneAndUpdate  (id, {
+      name,
+      description,
+      type,
+      sex,
+      size,
+      age,
+      status,
+      color,
+      breed,
+      dateofentry,
+      kidfriendly,
+      imageUrl: req.files,
+     // shelter: req.session.loggedInUser._id,
+}).then((newlyUpdatedAnimalFromDB) => {
+   
+    res.redirect("/shelters/animals");
+  })
+  .catch((error) =>
+    console.log(`Error while updating an animal: ${error}`)
+  );
+
+} )
+
+router.get("/delete/:id", async(req, res) => {
+
+    let id = req.params.id
+    
+    try {
+
+   await Animal.findByIdAndDelete( id ) 
+    
+    res.redirect("/shelters/animals")
+    
+    } 
+    
+    catch(e) { 
+        return console.log(e)
+    }
+    
+})
+
 
 module.exports = router;
